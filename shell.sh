bf1fa4ab5b5cdb161a34e4f7b2872313983dd115
Dinesh A <dineshmutthurasu@gmail.com>
2021-03-03 21:58:15 +0530

    testing with shell file

--- shell.sh
+++ shell.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-CHANGED_FILES=($(git diff --name-only --diff-filter=ACMRTUXB origin/main origin/action_test))
-echo "initial array ${CHANGED_FILES}"
+CHANGED_FILES=($(git diff --name-only --diff-filter=ACMRTUXB origin/main))
+echo "initial array ${CHANGED_FILES[*]}"
 for index in "${!CHANGED_FILES[@]}" ;
-  do [[ ${CHANGED_FILES[$index]} = !(*.js$|*.ts$|*.tsx$) ]] && unset -v 'CHANGED_FILES[$index]' ;
+  do [[ ${CHANGED_FILES[$index]} == !(*.js|*.jsx|*.ts|*.tsx) ]] && unset -v 'CHANGED_FILES[$index]' ;
   echo ${CHANGED_FILES[$index]}
 done
 CHANGED_FILES=("${CHANGED_FILES[@]}")
-echo "updated array ${CHANGED_FILES}"
+echo "updated array ${CHANGED_FILES[*]}"
 # if [[ -z ${CHANGED_FILES} ]]; then
 #   echo "::set-output name=files_changed::false"
 #   exit 0;
