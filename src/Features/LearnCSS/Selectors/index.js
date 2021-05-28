2151c1b22c5ea0930ab3c101238a3d34d79a22d8
Dinesh A <dineshmutthurasu@gmail.com>
2021-05-28 18:54:20 +0530

    updates in Selector

--- src/Features/LearnCSS/Selectors/index.js
+++ src/Features/LearnCSS/Selectors/index.js
@@ -1,13 +1,35 @@
 import "./index.css";
 
 export const template = `
-    <div>
+    <div class="top">
         <p data-para-number="1" >paragraph one</p>
-        <p id="middle-para-id" class="middle-para" data-para-number="2">paragraph two</p>
-        <p data-para-number="3">paragraph three</p>
+        <p id="middle-para-id" class="middle-para extra-class" data-para-number="2">paragraph two</p>
+        <p class="last-para" data-para-number="3">paragraph three</p>
+        <div>
+            <span id="span-one">span1</span>
+            <span id="span-two">
+                span2
+                <span>inner span</span>
+            </span>
+        </div>
+        <div>
+            <ul>
+                <li>list one</li>
+                <li>list two</li>
+            </ul>
+        </div>
+        <div>
+          <p>1 level deep</p>
+          <div>
+            <p>2 levels deep</p>
+            <div>
+              <p>3 levels deep</p>
+            </div>
+          </div>
+        </div>
     </div>
 `;
 
 export const initialize = () => {
-    // initialization function.
+    document.title = "Selectors : Learn CSS";
 };
