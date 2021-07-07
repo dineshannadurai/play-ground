01f2b092a815f9a3121a389b04d32d3da6cc5489
Dinesh A <dineshmutthurasu@gmail.com>
2021-07-07 09:36:02 +0530

    added and configured assets

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,10 @@ module.exports = {
                 test: /\.css$/,
                 use: ["style-loader", "css-loader"],
             },
+            {
+                test: /\.svg/,
+                type: "asset",
+            },
         ],
     },
     resolve: {
